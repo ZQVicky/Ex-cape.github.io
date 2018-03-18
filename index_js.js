960e065f81c8f35b9997f71cf3715ea0e5fdf54c
ZQVicky <32510491+ZQVicky@users.noreply.github.com>
2018-03-18 16:31:46 +0000

    Add files via upload

---
+++ index_js.js
@@ -0,0 +1,16 @@
+Qfast.add('widgets', { path: "http://hovertree.com/texiao/js/1/js/terminator2.2.min.js", type: "js", requires: ['fx'] });
+Qfast(false, 'widgets', function () {
+    K.tabs({
+        id: 'decoroll2', //焦点图包裹id
+        conId: "decoimg_a2", //大图域包裹id
+        tabId: "deconum2", //小圆点数字提示id
+        tabTn: "a",
+        conCn: '.decoimg_b2', //大图域配置class
+        auto: 1, //自动播放 1或0
+        effect: 'fade', //效果配置
+        eType: 'mouseover', // 鼠标事件
+        pageBt: true, //是否有按钮切换页码
+        bns: ['.prev', '.next'], //前后按钮配置class
+        interval: 3000// 停顿时间
+    })
+})
